b72a1e8172f15d9c5280990b42f49f4b6a01c552
uGabrielS2 <antonio.meireles1@aluno.ce.gov.br>
2025-09-01 12:03:34 -0300

    Commit avaliação prática - questões resolvidas

---
+++ q1.py
@@ -0,0 +1,11 @@
+# peso=float(input("Digite seu peso em kg: "))
+# altura=float(input("Digite sua altura em metros: "))
+# imc=peso / (altura * altura)
+# if imc < 18.5 :
+#     print(f"Seu IMC é: {imc}: abaixo do peso")
+# if 18.5 <= imc < 25 :
+#     print(f"Seu IMC é: {imc}: peso normal")
+# if 25 <= imc < 30 :
+#     print(f"Seu IMC é: {imc}; sobrepeso")
+# if imc >= 30 :
+#     print(f"Seu IMC é: {imc}: obesidade")
---
+++ q2.py
@@ -0,0 +1,6 @@
+# contador=0
+# alunos=int(input("Digite o número de alunos: "))
+# for contador in alunos :
+#     nota=int(input(f"Digite a nota de cada {contador}:"))
+#     media=contador=+1 / alunos
+# mediageral=media 
